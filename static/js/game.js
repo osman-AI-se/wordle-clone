64366a9daad6506d57dbf90e3385ed817b7d619e
Lunessyy <didar02@live.se>
2026-02-06 15:36:06 +0100

    Boxes created 5x6

--- static/js/game.js
+++ static/js/game.js
@@ -1,14 +1,46 @@
-const ROWS = 6;
-const COLS = 5;
-let currentRow = 0;
-let currentCol = 0;
-let secretWord = "hej";
+const state = {
+    grid: Array(6)
+          .fill()
+          .map(() => Array(5).fill('')),
+          currentRow: 0,
+          currentColumn: 0,
+};
 
 
-function initGame() 
+function createBox(container, row, column, letter = '')
+    {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.id = `box${row}${column}`;
+        box.textContent = letter;
+
+        container.appendChild(box);
+        return box;
+    }
+function drawGrid(container)
 {
-    console.log("Game has started")
-    console.log("secretWord: " , secretWord);
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    for(let i = 0; i < 6;i++)//antal rader
+    {
+        for(let j = 0; j < 5; j++) //antal kolumner 
+        {
+            createBox(grid,i,j);
+        }
+    }
+    container.appendChild(grid);
+}
+
+
+function initGame() //startar spelet
+{
+    const game = document.getElementById('game');
+    if (game) {
+        drawGrid(game);
+    } else {
+        console.error('Element with id="game" not found!');
+    }
 }
 
 document.addEventListener("DOMContentLoaded", initGame);
