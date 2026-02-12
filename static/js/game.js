425c87df4ce068515f4461bbce5ac83b5b52c9eb
Lunessyy <didar02@live.se>
2026-02-12 12:34:22 +0100

    new features were added, its now possible to guess a word

--- static/js/game.js
+++ static/js/game.js
@@ -1,10 +1,23 @@
+const dictionary = ['earth', 'forest', 'candy', 'schoo','house'];
 const state = {
+    secret: dictionary[Math.floor(Math.random()* dictionary.length)],
     grid: Array(6)
           .fill()
           .map(() => Array(5).fill('')),
           currentRow: 0,
           currentColumn: 0,
 };
+function updategrid(){
+    for(let i = 0; i < state.grid.length;i++)
+    {
+        for(let j = 0;j<state.grid[i].length;j++)
+        {
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+      
+    }
+}
 
 
 function createBox(container, row, column, letter = '')
@@ -32,18 +45,116 @@ function drawGrid(container)
     container.appendChild(grid);
 }
 
+function KeyboardEvents(guess)
+{
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+
+        if (key === 'Enter'){
+            if(state.currentColumn === 5){
+                const word = getCurrentWord();
+                if(isWordValid(word)){
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentColumn = 0;
+                }else{
+                    alert(`${word} Its not a valid word, try again`);
+                }
+            }
+
+        }
+        if (key === 'Backspace'){
+            removeLetter();
+        }
+        if (isLetter(key)){
+            addLetter(key);
+        }
+        updategrid();
+
+    };
+}
+function getCurrentWord(){
+    return state.grid[state.currentRow].reduce((prev,curr) => prev + curr);
+}
+function isWordValid(word){
+    return dictionary.includes(word);
+
+}
+function revealWord(guess){
+    const row = state.currentRow;
+
+    for(let i = 0; i < 5; i++){
+        const box = document.getElementById(`box${row}${i}`);
+        const letter = box.textContent;
+
+        if (letter === state.secret[i]){
+            box.classList.add('right');
+            
+        }
+        else if (state.secret.includes(letter)){
+            box.classList.add('wrong');
+        }
+        else {
+            box.classList.add('empty');
+        }
+    }
+    const isWinner = state.secret === guess;
+    const isOver = state.secret === 5;
+
+    if (isWinner){
+        alert('Congratulations you won!')
+    }
+    else if (isOver){
+        alert(`You lost, the correct word was ${state.secret}`)
+    }
+}
+function isLetter(key)
+{
+    return key.length === 1 && key.match(/[a-z]/i);
+}
+
+function addLetter(letter)
+{
+     if(state.currentColumn === 5) return;
+     state.grid[state.currentRow][state.currentColumn] = letter;
+     state.currentColumn++;
+}
+
+function removeLetter()
+{
+    if(state.currentColumn === 0) return;
+    state.grid[state.currentRow][state.currentColumn - 1] = '';
+    state.currentColumn--;
+}
 
 function initGame() //startar spelet
 {
     const game = document.getElementById('game');
-    if (game) {
-        drawGrid(game);
-    } else {
+    if(game)
+        {
+            drawGrid(game); 
+        }
+        
+    else 
+    { 
         console.error('Element with id="game" not found!');
     }
+
+    KeyboardEvents();
+    console.log(state.secret);
+
+
+   
 }
 
 document.addEventListener("DOMContentLoaded", initGame);
+
+
+
+
+
+
+
 
 /* ------------------  Hi Teams,  ------------------*/
 /* ------------------ I need something similar this block to connect backend -------------------
